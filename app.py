c1e901778c5dbf69bc522574accd6d8cb8ad9fb0
aledmorris <aled.morris@pexip.com>
2020-09-07 17:43:53 +1000

    commit on 'master'

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def index():
     
     extra_data = ""
     browser_advice = ""
+    pexip_interop_url = "https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser"
     
     if request.method == 'POST':
         user_agent_string = request.form['user_agent']
@@ -35,26 +36,26 @@ def index():
             # gneral browser version checks
             if "Safari" in str(ua_string.browser.family) and "ios" in str(ua_string.os.family.lower()):
                 if float(ua_string.browser.version_string[0:3]) < 11.2:
-                    extra_data = "Minimum Safari version for iOS is v11.2 (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    extra_data = "Minimum Safari version for iOS is v11.2."
             elif "Safari" in str(ua_string.browser.family) and "Mac" in str(ua_string.os.family.lower()):
                 if float(ua_string.browser.version_string[0:3]) < 11.1:
-                    extra_data = "Minimum Safari version is v11.1 (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    extra_data = "Minimum Safari version is v11.1."
             elif "Chrome" in str(ua_string.browser.family):
                 if float(ua_string.browser.version_string[0:3]) < 61:
-                    browser_advice = "Minimum Chrome version is v61 (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    browser_advice = "Minimum Chrome version is v61."
             elif "Firefox" in str(ua_string.browser.family):
                 if float(ua_string.browser.version_string[0:3]) < 68:
-                    browser_advice = "Minimum Firefox version is v68 (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    browser_advice = "Minimum Firefox version is v68."
             elif "Edge" in str(ua_string.browser.family):
                 if float(ua_string.browser.version_string[0:3]) < 41:
-                    browser_advice = "Minimum Edge version is v41, Chromium or HTML versions (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    browser_advice = "Minimum Edge version is v41, Chromium or HTML versions."
             elif "Opera" in str(ua_string.browser.family):
                 if float(ua_string.browser.version_string[0:3]) < 53:
-                    browser_advice = "Minimum Opera version is v53 (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                    browser_advice = "Minimum Opera version is v53."
             elif "IE" in str(ua_string.browser.family):
-                browser_advice = "Internet Explorer is not supported, please use a modern browser (https://docs.pexip.com/admin/interoperability.htm?Highlight=interoperability#browser)"
+                browser_advice = "Internet Explorer is not supported, please use a modern browser."
             
             
-    return render_template("index.html", message=error, browser = browser_data, os = os_data, device = device_data, extra = extra_data, browser_info = browser_advice)
+    return render_template("index.html", message=error, browser = browser_data, os = os_data, device = device_data, extra = extra_data, browser_info = browser_advice, pex_url = pexip_interop_url)
 
 
